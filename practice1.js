42a16afe036e04ffbbaf1f311034faf0da82a529
rk <rk@rknoMacBook-Pro.local>
2020-06-19 00:20:17 +0900

    [modify]fizzbuzz実装

---
+++ practice1.js
@@ -0,0 +1,58 @@
+// 実行ボタンのIDを取得
+const btn = document.getElementById('btn');
+
+// クリックイベント
+btn.addEventListener('click', () => 
+{
+  // DOM
+  const fizz_num = document.getElementById('fizz_num');
+  const fizz = fizz_num.value;
+  const buzz_num = document.getElementById('buzz_num');
+  const buzz = buzz_num.value;
+  const output = document.getElementById('output');
+
+  // 値を代入する変数を宣言
+  let val = '';
+
+  // fizzとbuzzの値が空欄,文字列,小数が存在した場合、outputの中身を空にして、注意文を表示する
+  if (!fizz || fizz.match(/^\D+$/) || fizz.match(/^-?[0-9]+\.[0-9]+$/) || !buzz || buzz.match(/^\D+$/) || buzz.match(/^-?[0-9]+\.[0-9]+$/)) {
+    output.innerHTML = '';
+    output.innerHTML = '<p>整数値を入力してください</p>'
+
+  // fizzとbuzzの値が整数値の場合
+  } else {
+
+    // outputの中身を空にする
+    output.innerHTML = '';
+
+      // 変数iを宣言し、iに1を代入。iが100未満なら+1を繰り返す
+      for (let i = 1; i < 100; i++) {
+
+        // iがfizzとbuzzの倍数なら、valに文字列とiを代入
+        if (i % fizz == 0 && i % buzz == 0) {
+          val = 'FizzBuzz' + i;
+
+        // iがfizzの倍数なら、valに文字列とiを代入
+        } else if (i % fizz == 0) {
+          val = 'Fizz' + i;
+
+        // iがbuzzの倍数なら、valに文字列とiを代入
+        } else if (i % buzz == 0) {
+          val = 'Buzz' + i;
+        
+        // iがfizzとbuzzの倍数でないなら、valを空にする
+        } else {
+          val = '';
+        }
+
+        // li要素を作成する定数liを宣言
+        const li = document.createElement('li');
+
+        // outputの子要素にliを追加
+        output.appendChild(li);
+
+        // liのテキストにvalを代入
+        li.textContent = val;      
+      }
+  } 
+})
