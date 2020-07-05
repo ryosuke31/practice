99159dc5b6a03c21ad6b0afa5d169bbd98ef97b6
rk <rk@rknoMacBook-Pro.local>
2020-07-05 23:36:18 +0900

    [modify]削除ボタンの実装とリファクタリング

--- practice3/practice3.js
+++ practice3/practice3.js
@@ -1,22 +1,13 @@
-// 追加ボタンの要素を取得
+// 追加タスクのオブジェクトをまとめる配列を作成
+const todos = [];
+
+// DOM
 const add = document.getElementById('add');
+const addComment = document.getElementById('add_comment');
+const tableList = document.getElementById('table_list');
 
 // 追加ボタンクリック時のイベント
 add.addEventListener('click', () => {
-
-  // DOM
-  const addComment = document.getElementById('add_comment');
-  const tableList = document.getElementById('table_list');
-  const workBtn = document.createElement('button');
-  const deleteBtn = document.createElement('button');
-  const tr = document.createElement('tr');
-  const tdNumber = document.createElement('td');
-  const tdComment = document.createElement('td');
-  const tdStatus = document.createElement('td');
-  const tdDelete = document.createElement('td');
-  
-  // 追加タスクのオブジェクトをまとめる配列を作成
-  const todos = [];
 
   // 追加タスクをオブジェクトで作成
   const todo = {
@@ -33,43 +24,63 @@ add.addEventListener('click', () => {
   if (todo) {
     todos.push(todo);
     addComment.value = '';
+    displayTodos();
   }
-
-
-  // タスク一覧を表示するための関数を宣言
-  const displayTodos = () => {
-
-    // todos内の配列を一つずつ取り出す
-    todos.forEach((todo) => {
-      
-      // ID用tdにクラスを追加
-      tdNumber.classList.add('id_number');
-
-      // ID用tdにクラスの配列要素数の文字列を代入
-      tdNumber.textContent = document.getElementsByClassName('id_number').length;
-
-      // コメント用tdにtodoのtaskを代入
-      tdComment.textContent = todo.task;
-
-      // 状態用ボタンにtodoのstatusを代入
-      workBtn.textContent = todo.status;
-
-      // 削除ボタンの文字列代入
-      deleteBtn.textContent = '削除';
-
-      // table_listの子要素にID,コメント,状態,削除を追加
-      tdStatus.appendChild(workBtn);
-      tdDelete.appendChild(deleteBtn);
-      tr.appendChild(tdNumber);
-      tr.appendChild(tdComment);
-      tr.appendChild(tdStatus);
-      tr.appendChild(tdDelete);
-      tableList.appendChild(tr);
-    })
-  }
-
-  // 関数呼び出し
-  displayTodos();
 })
 
+// タスク一覧を表示するための関数を宣言
+const displayTodos = () => {
 
+  // 一度tableListを空にする
+  tableList.innerHTML = '';
+  
+  // todos内の配列を一つずつ取り出す
+  todos.forEach((todo) => {
+    
+    // DOM
+    const workBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+    const tr = document.createElement('tr');
+    const tdNumber = document.createElement('td');
+    const tdComment = document.createElement('td');
+    const tdStatus = document.createElement('td');
+    const tdDelete = document.createElement('td');
+
+    // ID用tdにクラスを追加
+    tdNumber.classList.add('id_number');
+
+    // ID用tdにクラスの配列要素数の文字列を代入
+    tdNumber.textContent = document.getElementsByClassName('id_number').length;
+
+    // 削除用tdにクラスを追加
+    tdDelete.classList.add('delete');
+
+    // 削除ボタンにクラスを追加
+    deleteBtn.classList.add('delete_btn');
+
+    // コメント用tdにtodoのtaskを代入
+    tdComment.textContent = todo.task;
+
+    // 状態用ボタンにtodoのstatusを代入
+    workBtn.textContent = todo.status;
+
+    // 削除ボタンの文字列代入
+    deleteBtn.textContent = '削除';
+
+    // 削除ボタンをクリックしたら、その行のタスクを削除
+    deleteBtn.addEventListener('click', () => {
+      let index = tr.rowIndex - 1;
+      todos.splice(index, 1);
+      displayTodos();
+    })
+
+    // table_listの子要素にID,コメント,状態,削除を追加
+    tdStatus.appendChild(workBtn);
+    tdDelete.appendChild(deleteBtn);
+    tr.appendChild(tdNumber);
+    tr.appendChild(tdComment);
+    tr.appendChild(tdStatus);
+    tr.appendChild(tdDelete);
+    tableList.appendChild(tr);
+  })
+}
